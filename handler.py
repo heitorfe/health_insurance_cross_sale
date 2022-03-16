dfc09779bf5a47b5e54e05125850a5e92b352eca
heitorfe <heitorfelix@yahoo.com.br>
2022-03-16 19:19:57 -0300

    handler

--- handler.py
+++ handler.py
@@ -13,34 +13,34 @@ app = Flask(__name__)
 def healthinsurance_predict():
     test_json = request.get_json()
     if test_json: #there is data
-        if isinstance(test_jason, dict):  #unique example 
+        if isinstance(test_json, dict):  #unique example 
             test_raw = pd.DataFrame(test_json, index=[0])
         
         else:                             #multiple example
             test_raw = pd.DataFrame(test_json, columns = test_json[0].keys())
             
-            #Initialize HealthInsurance Class
-            pipeline = HealthInsurance()
+        #Initialize HealthInsurance Class
+        pipeline = HealthInsurance()
             
-            #data cleaning
-            data = data_cleaning(test_raw)
+        #data cleaning
+        data = data_cleaning(test_raw)
+        
+        #feature engineering
+        data = feature_engineering(data)
+        
+        #data preparation
+        data = data_preparation(data)
+        
+        #prediction
+        df_response = get_prediction(model, test_raw, data)
             
-            #feature engineering
-            data = feature_engineering(data)
-            
-            #data preparation
-            data = data_preparation(data)
-            
-            #prediction
-            df_response = get_prediction(model, test_raw, data)
-            
-            return df_response
+        return df_response
         
     
     else:
         return Response('{}', status = 200, mimetype = 'application/json')
 
-if __name__ == 'main':
+if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
     app.run('0.0.0.0', port=port)
             
