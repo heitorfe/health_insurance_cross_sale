bc9d5c5ccffd30a7537a5f2b1388f99731505bb8
heitorfe <heitorfelix@yahoo.com.br>
2022-03-16 19:28:42 -0300

    changes

--- handler.py
+++ handler.py
@@ -42,7 +42,7 @@ def healthinsurance_predict():
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
-    app.run('0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
             
             
             
